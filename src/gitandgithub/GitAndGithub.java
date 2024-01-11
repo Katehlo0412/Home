acf19e4cbff6043c651db9c0c8526e0fe2e9400e
Katehlo0412 <154536285+Katehlo0412@users.noreply.github.com>
2024-01-11 16:22:14 +0100

    Update GitAndGithub.java
    Esta modificación se hizo desde github

--- src/gitandgithub/GitAndGithub.java
+++ src/gitandgithub/GitAndGithub.java
@@ -6,6 +6,8 @@ public class GitAndGithub {
   
     public static void main(String[] args) {
         System.out.println("Esta linea de codigo viene de local");
+        System.out.println("Esta linea de código se hizo desde github");
+      
       
     }
     
