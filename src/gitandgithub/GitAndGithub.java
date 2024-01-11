f7315f854ca5e858bc902f34881fe28a2ed5f1d8
Katehlo0412 <154536285+Katehlo0412@users.noreply.github.com>
2024-01-11 16:28:29 +0100

    Update GitAndGithub.java.ultimoComentario

--- src/gitandgithub/GitAndGithub.java
+++ src/gitandgithub/GitAndGithub.java
@@ -6,8 +6,8 @@ public class GitAndGithub {
   
     public static void main(String[] args) {
         System.out.println("Esta linea de codigo viene de local");
-        System.out.println("Esta linea de código se hizo desde github");
-      
+       
+      //he borrado una linea de codigo
       
     }
     
