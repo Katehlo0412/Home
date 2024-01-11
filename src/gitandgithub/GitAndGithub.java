798e11dedaacec0085fb0f40ffa7d233c7f75e97
Fatima <Fatima@DESKTOP-Q8U551D>
2024-01-11 15:56:22 +0100

    Inicialización de mi nuevo proyecto

---
+++ src/gitandgithub/GitAndGithub.java
@@ -0,0 +1,11 @@
+
+package gitandgithub;
+
+public class GitAndGithub {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Esta linea de codigo viene de local");
+    }
+    
+}
